603454228e514a0c5df6ce8272209f3ffee1d35d
Amar <jeetamar496@gmail.com>
2024-11-19 16:47:40 +0530

    build errors ignored

--- next.config.ts
+++ next.config.ts
@@ -6,3 +6,21 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  swcMinify: false,
+}
